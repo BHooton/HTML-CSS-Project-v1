11b03d6ae9b775aed05674bf9e006b1152457a85
Braden Hooton <39040355+BHooton@users.noreply.github.com>
2018-11-04 12:29:26 -0700

    Add files via upload

---
+++ SuccessPage.php
@@ -0,0 +1,16 @@
+<html>
+	<title>
+	Order Confirmation
+	</title>
+		<body>
+			<img src="Cinemark_logo.png" width='745' height='76'>
+			<p align="center"><br>
+			<b>Ticket Order Successfully Processed</b>
+			</p>
+			<p>
+				Your order was processed successfully. You can pick up your tickets at the Box Office. 
+			</p>
+			<form action="MovieList.php" align="center">
+				<input type="submit" value="Home">
+		</body>
+</html>
