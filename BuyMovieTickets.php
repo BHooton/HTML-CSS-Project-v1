921eb5f2b7d3d874d8626b1cb5a82c32cc84b49b
Braden Hooton <39040355+BHooton@users.noreply.github.com>
2018-11-04 12:28:56 -0700

    Add files via upload

---
+++ BuyMovieTickets.php
@@ -0,0 +1,104 @@
+<HTML>
+	<Title>
+	</Title>
+		<h1>
+		</h1>
+		<body>
+		<br>
+			<img src="Cinemark_logo.png" width='745' height='76'>
+			<h2> Purchase Tickets </h2>
+			<p> Please fill out the form below.</p>
+			<form>
+				Movie:<br>
+				<input list="movie">
+					<datalist id = "movie">
+						<option value ="Aquaman">
+						<option value ="Creed II">
+						<option value ="Fantastic Beasts: Crimes of Grindlewald">
+						<option value ="Mission Impossible">
+						<option value ="Predator (2018)">
+						<option value ="Robin Hood (2018)">
+						<option value ="Venom">
+					</datalist>
+				<br>
+				Number of Tickets:<br>
+				<input list="tickets">
+					<datalist id ="tickets">
+						<option value = "1">
+						<option value = "2">
+						<option value = "3">
+						<option value = "4">
+						<option value = "5">
+						<option value = "6">
+						<option value = "7">
+						<option value = "8">
+						<option value = "9">
+						<option value = "10">
+						<option value = "11">
+						<option value = "12">
+						<option value = "13">
+						<option value = "14">
+						<option value = "15">
+					</datalist>
+				<br>
+				Auditorium:<br>
+				<input list="Auditorium">
+					<datalist id ="Auditorium">
+						<option value = "1">
+						<option value = "2">
+						<option value = "3">
+						<option value = "4">
+						<option value = "5">
+						<option value = "6">
+						<option value = "7">
+						<option value = "8">
+						<option value = "9">
+						<option value = "10">
+						<option value = "11">
+						<option value = "12">
+						<option value = "13">
+						<option value = "14">
+						<option value = "15">
+						<option value = "16">
+						<option value = "17">
+						<option value = "18">
+						<option value = "19">
+						<option value = "20">
+						<option value = "21">
+						<option value = "22">
+						<option value = "23">
+						<option value = "24">
+						<option value = "25">
+						<option value = "26">
+					</datalist>
+				<br>
+				Showtime:<br>
+				<input list="ShowTime">
+					<datalist id ="ShowTime">
+						<option value = "12:30PM">
+						<option value = "12:45PM">
+						<option value = "12:50PM">
+						<option value = "1:50PM">
+						<option value = "2:20PM">
+						<option value = "2:45PM">
+						<option value = "3:15PM">
+						<option value = "3:50PM">
+						<option value = "4:25PM">
+						<option value = "5:20PM">
+						<option value = "5:55PM">
+						<option value = "6:50PM">
+						<option value = "7:15PM">
+						<option value = "7:30PM">
+						<option value = "8:10PM">
+						<option value = "8:45PM">
+						<option value = "9:05PM">
+						<option value = "10:15PM">
+						<option value = "10:45PM">
+						<option value = "11:45PM">
+					</datalist>
+			</form>
+			<form action="PaymentProcess.php">
+				<input type="submit" value ="Payment">
+			</form>	
+		</body>
+</HTML>
